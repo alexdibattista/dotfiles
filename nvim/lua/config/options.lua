95ba46b17ef1d37c0174cc2efad7d90f6878a61d
Alex Di Battista <alex.dibattista@gmail.com>
2023-07-27 14:23:47 -0400

    nvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+  },
+}
