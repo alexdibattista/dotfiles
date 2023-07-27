a6f968582a1e460e0ee328f575d9f650b7362710
Alex Di Battista <alex.dibattista@gmail.com>
2023-07-27 14:34:44 -0400

    adds remap caps to esc

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+hidutil property --set {"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc": 0x700000039, "HIDKeyboardModifierMappingDst": 0x700000029}]}
+
 xcode-select --install
+
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 
 curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
@@ -18,4 +21,4 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.tmux.conf.local ~/.tmux.conf.local
 ln -s ~/dotfiles/nvim/ftplugin ~/.config/nvim/
 ln -s ~/dotfiles/nvim/ftdetect ~/.config/nvim/
-ln -s ~/dotfiles/alacritty/.alacritty.yml  ~/.config/alacritty
+ln -s ~/dotfiles/alacritty/.alacritty.yml ~/.config/alacritty
